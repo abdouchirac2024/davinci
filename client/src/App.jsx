221e13b15696667de8584ca2c1da1524ef5dba86
abdouchirac2024 <abdouchirac411@gmail.com>
2024-07-03 17:41:25 +0200

    add call to action to the post page

---
+++ client/src/App.jsx
@@ -0,0 +1,41 @@
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import SigIn from './pages/SigIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
+import Header from './components/Header'
+import CreatePost from './pages/CreatePost'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
+
+
+export default function App() {
+  return (
+    <BrowserRouter >
+
+    <Header />
+    
+    <Routes>
+      
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sigin-in' element={<SigIn />} />
+      <Route path='/sigin-up' element={<SignUp />} />
+      <Route element={<PrivateRoute />}></Route>
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/projects' element={<Projects  />} />
+      <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+          <Route path='/post/:postSlug' element={<PostPage />} />
+        </Route>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  )
+}
