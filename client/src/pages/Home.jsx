11d01502422acd36170227d2754ba67688f03928
abdouchirac2024 <abdouchirac411@gmail.com>
2024-08-31 19:50:37 +0200

    valide

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,7 @@
-//import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
-import { useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
-import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -20,17 +18,7 @@ export default function Home() {
   return (
     <div>
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-       
-        <h2 className="text-4xl md:text-5xl xl:text-7xl">
-        Welcome to{" "}
-          <TypeAnimation
-            sequence={["DAVINCI Blogs", 1000, "", 1000, "Solutions", 1000]}
-            wrapper="span"
-            speed={20}
-            className="text-blue-500 inline-block"
-            repeat={Infinity}
-          />{" "}
-        </h2>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
         <Link
           to='/search'
           className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
@@ -41,7 +29,6 @@ export default function Home() {
       <div className='p-3 bg-amber-100 dark:bg-slate-700'>
         <CallToAction />
       </div>
-
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
@@ -59,6 +46,7 @@ export default function Home() {
             </Link>
           </div>
         )}
+        
       </div>
     </div>
   );
